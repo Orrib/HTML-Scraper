6f532fa708804eef615d078a7b6ef2953fc472c0
Orrib <46468266+Orrib@users.noreply.github.com>
2020-04-19 15:38:31 -0400

    Add files via upload

---
+++ Graphics and Networking - Scraper/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Graphics_and_Networking___Scraper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 347);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(683, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Scrape From https://www.oceannetworks.ca/news/stories";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(24, 51);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(683, 290);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(225, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Search (seperate multiple terms with a comma)";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(24, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(683, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "Ocean,Nature,Pollution";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Graphics and Networking - Scraper/Form1.cs
@@ -0,0 +1,115 @@
+﻿using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
+using AngleSharp.Html.Parser;
+using AngleSharp.Text;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Windows.Forms;
+
+// Author: Orrett Coke
+// Date: 04-19-2020
+// Based on webscrapping tutorial from https://dev.to/rachelsoderberg/create-a-simple-web-scraper-in-c-1l1m
+
+namespace Graphics_and_Networking___Scraper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            ScrapeWebsite();
+        }
+
+
+        private string Title { get; set; }
+        private string Url { get; set; }
+        private string siteUrl = "https://www.oceannetworks.ca/news/stories";
+
+        public string[] GetQueryTerms()
+        {
+            // Return terms from search box
+            return textBox1.Text.Split(',');
+        }
+
+        internal async void ScrapeWebsite()
+        {
+            CancellationTokenSource cancellationToken = new CancellationTokenSource();
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage request = await httpClient.GetAsync(siteUrl);
+            cancellationToken.Token.ThrowIfCancellationRequested();
+
+            Stream response = await request.Content.ReadAsStreamAsync();
+            cancellationToken.Token.ThrowIfCancellationRequested();
+
+            HtmlParser parser = new HtmlParser();
+            IHtmlDocument document = parser.ParseDocument(response);
+
+            // Parse results from the initial bulk document scrape
+            GetScrapeResults(document);
+        }
+
+        private void GetScrapeResults(IHtmlDocument document)
+        {
+            IEnumerable<IElement> articleLink = null;
+
+            // Use the query terms to find results in HTML and print each
+            foreach (var term in GetQueryTerms())
+            {
+                // Find each element using specified class names and query terms
+                articleLink = document.All.Where(x =>
+                    x.ClassName == "views-field views-field-nothing" &&
+                    (x.ParentElement.InnerHtml.Contains(term) || x.ParentElement.InnerHtml.Contains(term.ToLower()))).Skip(1);
+
+                // Print results if found
+                if (articleLink.Any())
+                {
+                    PrintResults(articleLink);
+                }
+            }
+        }
+
+        public void PrintResults(IEnumerable<IElement> articleLink)
+        {
+            // Clean each scrapped result and print to the textbox
+            foreach (var element in articleLink)
+            {
+                CleanUpResults(element);
+
+                // Print each result to the text box
+                richTextBox1.AppendText($"{Title} - {Url}{Environment.NewLine}");
+            }
+        }
+
+        private void CleanUpResults(IElement result)
+        {
+            // Clean the results by replacing extra unnecessary HTML chunks
+            string htmlResult = result.InnerHtml.ReplaceFirst("        <span class=\"field-content\"><div><a href=\"", @"https://www.oceannetworks.ca");
+            htmlResult = htmlResult.ReplaceFirst("\">", "*");
+            htmlResult = htmlResult.ReplaceFirst("</a></div>\n<div class=\"article-title-top\">", "-");
+            htmlResult = htmlResult.ReplaceFirst("</div>\n<hr></span>  ", "");
+
+            // Split each result into a article title and link. This allows for easing printing in PrintResults()
+            SplitResults(htmlResult);
+        }
+
+        private void SplitResults(string htmlResult)
+        {
+            string[] splitResults = htmlResult.Split('*');
+            Url = splitResults[0];
+            Title = splitResults[1];
+        }
+    }
+}
